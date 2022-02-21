424938a39389c257a63df029d69e9e0f7b91c702
Jerish Balakrishnan <jerishbalakrishnan@gmail.com>
2022-02-22 04:39:31 +0530

    heroku deployment

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-xl navbar-dark bg-dark" aria-label="Sixth navbar example">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel Framework') }}</a>
+        <a class="navbar-brand" href="https://laravel-application-demo.herokuapp.com/public/">{{ config('app.name', 'Laravel Framework') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,13 +8,13 @@
         <div class="collapse navbar-collapse" id="navbarsExample06">
         <ul class="navbar-nav me-auto mb-2 mb-xl-0">
             <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/">Home</a>
+            <a class="nav-link" aria-current="page" href="https://laravel-application-demo.herokuapp.com/public/">Home</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
+            <a class="nav-link" href="https://laravel-application-demo.herokuapp.com/public/about">About</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="/services">Services</a>
+            <a class="nav-link" href="https://laravel-application-demo.herokuapp.com/public/services">Services</a>
             </li>
         </ul>
         </div>

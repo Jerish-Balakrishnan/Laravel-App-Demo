947f02337f3031ddd3d1bc3df321bdbfa75cac87
Jerish Balakrishnan <jerishbalakrishnan@gmail.com>
2022-02-22 04:04:56 +0530

    Laravel App

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-xl navbar-dark bg-dark" aria-label="Sixth navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel Framework') }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample06">
+        <ul class="navbar-nav me-auto mb-2 mb-xl-0">
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/services">Services</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This is a PHP Laravel Framework Demo</p>
+@endsection
+
